0bf1b912074582e72c3127c98b7a9bdad852b75b
Jackson Voigt <jackson.voigt@cerner.com>
2017-06-29 13:30:32 -0500

    Initial Commit

---
+++ nethack_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  nethack_swift
+//
+//  Created by Voigt,Jackson on 6/29/17.
+//  Copyright © 2017 Voigt,Camden. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
